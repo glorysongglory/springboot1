1b82faf6fc45674d482c44a2daa7bd6a5c9680be
sodbvi <317878410@qq.com>
2018-01-15 17:23:05 +0800

    spring httpsession redis

--- src/main/java/com/sodbvi/springboot/Springboot1Application.java
+++ src/main/java/com/sodbvi/springboot/Springboot1Application.java
@@ -6,10 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.RedisFlushMode;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableConfigurationProperties({ConfigBean.class})
 @EnableScheduling
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800,redisNamespace = "fuck",redisFlushMode = RedisFlushMode.ON_SAVE)
 public class Springboot1Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/sodbvi/springboot/web/Test005HttpSessionRedisController.java
@@ -0,0 +1,35 @@
+package com.sodbvi.springboot.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/test005")
+public class Test005HttpSessionRedisController {
+
+    @RequestMapping(value = "/first", method = RequestMethod.GET)
+    public Map<String, Object> firstResp (HttpServletRequest request){
+        Map<String, Object> map = new HashMap<>();
+        request.getSession().setAttribute("request Url", request.getRequestURL());
+        map.put("sessionId", request.getSession().getId());
+        map.put("request Url", request.getRequestURL());
+        request.getSession().setAttribute("map",map);
+        return map;
+    }
+
+    @RequestMapping(value = "/sessions", method = RequestMethod.GET)
+    public Object sessions (HttpServletRequest request){
+        Map<String, Object> map = new HashMap<>();
+        map.put("sessionId", request.getSession().getId());
+        map.put("message", request.getSession().getAttribute("map"));
+        return map;
+    }
+}
